8f357d1bf46ae40d7865b67521578bb8082c5c2b
Ayden Shankman <aydenshankman1@gmail.com>
2022-02-28 19:59:16 -0500

    formatting

--- proj1.py
+++ proj1.py
@@ -1,16 +1,26 @@
 from nltk import download
 from nltk.tokenize import sent_tokenize, word_tokenize
-download("punkt")
+#download("punkt")
+#download("stopwords")
+#from nltk.corpus import stopwords
 from array import *
 from collections import defaultdict
 import math
+from os.path import dirname
+#stop_words = set(stopwords.words("english"))
 
-
-subjects = defaultdict(lambda : defaultdict(lambda: defaultdict(int)))
-subjectData = defaultdict(lambda : defaultdict(int))
+subjects = defaultdict(lambda : defaultdict(lambda: defaultdict(int))) # Holds each subject and its words and word probabilities
+subjectData = defaultdict(lambda : defaultdict(int)) # Holds prob and count of each subject
 totalFiles = 0
 
-with open("./TC_provided/corpus1_train.labels") as f1:
+print("Input Training Document:")
+train = input()
+print("Input Test Document:")
+test = input()
+
+# ---------------- Training ----------------
+
+with open(train) as f1:
     links = f1.readlines()
     for link in links:
 
@@ -18,7 +28,7 @@ with open("./TC_provided/corpus1_train.labels") as f1:
         subjectData[subject]['count'] += 1
         totalFiles += 1
 
-        s = "./TC_provided" + link.split()[0][1:]
+        s = dirname(train) + link.split()[0][1:]
 
         with open(s) as f2:
             
@@ -38,41 +48,42 @@ for sub in subjectData:
     
     for word in subjects[sub]:
         subjects[sub][word]['prob'] = (subjects[sub][word]['count'])/(subjectData[sub]['count'])
-        
 
 
+# ---------------- Testing ----------------
 
+guesses = defaultdict(int)
 
-guesses = []
-
-with open("./TC_provided/corpus1_test.list") as f1:
+with open(test) as f1:
     links = f1.readlines()
     for link in links:
         possible_sub = defaultdict(lambda: 0)
 
-        s = "./TC_provided" + link[1:-1]
+        s = dirname(train) + link.split()[0][1:]
         with open(s) as f2:
             words = word_tokenize(f2.read())
+            
             for sub in subjects:
                 possible_sub[sub] = math.log(subjectData[sub]['prob'])
+                
                 for word in words:
                     if(word.isalpha()):
                         if(word in subjects[sub]):
                             possible_sub[sub] += math.log(subjects[sub][word]['prob'])
                         else:
                             possible_sub[sub] += math.log(.00155)
-            guesses.append(max(possible_sub, key = possible_sub.get))
+
+            guesses[link.split()[0]] = max(possible_sub, key = possible_sub.get)
+
         f2.close()
-
-
     f1.close()
 
-    correct = 0
+# ---------------- Writing ----------------
 
-    with open("./TC_provided/corpus1_test.labels") as f1:
-        links = f1.readlines()
-        for i in range(443):
-            if (links[i].split()[1] == guesses[i]):
-                correct += 1
+print("Input Output Document:")
+out = input()
 
-    print(correct/443)
+f = open(out, "w")
+for guess in guesses:
+    f.write(guess + " " + str(guesses[guess]) + '\n')
+f.close()
