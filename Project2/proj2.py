aa88c4a97009a96fd1f74fb8792e3a3ce70e2062
Shankface <aydenshankman1@gmail.com>
2022-03-28 16:27:33 -0400

    parse

--- Project2/proj2.py
+++ Project2/proj2.py
@@ -1,15 +1,19 @@
+# NLP Projext 2
+# Sentence Parser
+# Ayden Shankman
 
 from array import *
 from collections import defaultdict
 import sys
 
-#path = sys.argv[1]
+# ---------- Loading Grammar ----------
+path = sys.argv[1]
 
 print("Loading grammar...")
 nonTerms = defaultdict(list)
 Terms = defaultdict(list)
 
-with open('cnf.txt') as f:
+with open(path) as f:
     lines = f.readlines()
 
     for line in lines:
@@ -23,64 +27,46 @@ with open('cnf.txt') as f:
         else:
             Terms[line.split()[2]].append(A)
 
-s = 'i book the flight to houston'
-numWords = len(s.split())
 
-table = [[[] for x in range(numWords)] for x in range(numWords)]
+# ---------- CKY Algorithm ----------
+def CKY(s):
+    numWords = len(s.split())
 
-for i in range(numWords):
-    w = s.split()[i]
-    for A in Terms[w]:
-        table[i][i].append((A,(i,i,w,0),(i,i, w,0)))
+    table = [[[] for x in range(numWords)] for x in range(numWords)]
 
-#for n in range(numWords):
-#    for m in range(numWords):
-#        print(table[n][m], end= " ")
-#    print('\n')
+    for i in range(numWords):
+        w = s.split()[i]
+        for A in Terms[w]:
+            table[i][i].append((A,(i,i,w,0),(i,i, w,0)))
 
-for i in range(1,numWords):
-    for j in range(i,numWords):
-        x = j-i
-        y = j
+    for i in range(1,numWords):
+        for j in range(i,numWords):
+            x = j-i
+            y = j
 
-        #print("box = " + str(x) + "," + str(y))
+            for k in range(i):
+                x1 = x
+                y1 = y - (i-k)
 
-        for k in range(i):
-            x1 = x
-            y1 = y - (i-k)
-            
-            x2 = x + k + 1
-            y2 = y
+                x2 = x + k + 1
+                y2 = y
 
-            #print("current = " + str(x1) + "," + str(y1) + " -> " + str(x2) + "," + str(y2))
+                for n in range(len(table[x1][y1])):
+                    for m in range(len(table[x2][y2])):
+                    
+                        a = table[x1][y1][n][0]
+                        b = table[x2][y2][m][0]
 
-            for n in range(len(table[x1][y1])):
-                for m in range(len(table[x2][y2])):
-    
-                    a = table[x1][y1][n][0]
-                    b = table[x2][y2][m][0]
-                    #print(a,b)
-                    if (a + " " + b) in nonTerms:
-                        #print("Match: " + a + ' ' + b + ' -> ' + str(nonTerms[a + " " + b]))
-
-                        for item in nonTerms[a + " " + b]:
-                            table[x][y].append(((item,(x1,y1,a,n),(x2,y2,b,m))))
-
-                        
-                        #for n in range(numWords):
-                        #    for m in range(numWords):
-                        #        print(table[n][m], end= " ")
-                        #    print('\n')
-
-#for i in range(numWords):
-#    for j in range(numWords):
-#        print(i,j,end = " ")
-#        print(table[i][j])
-#    print('\n')
+                        if (a + " " + b) in nonTerms:
+                            for item in nonTerms[a + " " + b]:
+                                table[x][y].append(((item,(x1,y1,a,n),(x2,y2,b,m))))
+    return table
 
 
-def parse (x,y,z):
-    t = table[x][y][z][0]
+# ---------- Parsing Algorithm ----------
+def parse (x,y,z,tree, level = 1):
+    term = table[x][y][z][0]
+    pt = ''
 
     x1 = table[x][y][z][1][0]
     y1 = table[x][y][z][1][1]
@@ -92,30 +78,45 @@ def parse (x,y,z):
     z2 = table[x][y][z][2][3]
     
     if(term1.islower()):
-        return "[" + t + " " + term1 + ']'
-    return '[' + t + " " + parse(x1,y1,z1) + " " + parse(x2,y2,z2) +  "]"
+        return [("[" + term + " " + term1 + ']'),("[" + term + " " + term1 + ']')]
 
-def parseTree (x,y,z,level = 1):
-    t = table[x][y][z][0]
-
-    x1 = table[x][y][z][1][0]
-    y1 = table[x][y][z][1][1]
-    z1 = table[x][y][z][1][3]
-    term1 = table[x][y][z][1][2]
+    p = '[' + term + " " + parse(x1,y1,z1,tree)[0] + " " + parse(x2,y2,z2,tree)[0] +  "]"
     
-    x2 = table[x][y][z][2][0]
-    y2 = table[x][y][z][2][1]
-    z2 = table[x][y][z][2][3]
+    if(tree == 'y'):
+        pt = '[' + term + "\n" + '   '*level + parse(x1,y1,z1,tree,level+1)[1] + "\n" + '   '*level + parse(x2,y2,z2,tree,level+1)[1] + "\n" + '   '*(level-1)  + ']'
 
-    if(term1.islower()):
-        return "[" + t + " " + term1 + ']'
-    s = '[' + t + '\n' + '   '*level + parseTree(x1,y1,z1,level+1) + '\n' + '   '*level + parseTree(x2,y2,z2,level+1) + '\n' + '   '*(level-1)  + ']'
-    return s 
+    return[p,pt]
 
-x = 1
-for i in range(len(table[0][numWords-1])):
-    if(table[0][numWords-1][i][0] == 'S'):
-        print("Valid parse #" + str(x))
-        print(parse(0,numWords-1,i),'\n')
-        print(parseTree(0,numWords-1,i),'\n')
-        x+=1
+
+# ---------- Inputting Sentences ----------
+tree = input("Do you want textual parse trees to be displayed (y/n)?: ")
+s = ''
+while(s != 'quit'):
+    s = input("Enter a sentence: ")
+    if(s == 'quit'):
+        print('Goodbye!')
+        break
+
+    x = 0
+    table = CKY(s)
+    numWords = len(s.split())
+    valid = False
+
+    for i in range(len(table[0][numWords-1])):
+        if(table[0][numWords-1][i][0] == 'S'):
+            if(not valid):
+                print('VALID SENTENCE\n')
+            x+=1
+            print("Valid parse #" + str(x))
+            t = parse(0,numWords-1,i,tree)
+            print(t[0] + '\n' + t[1] + '\n')
+    if(x == 0):
+        print('NOT A VALID SENTENCE\n')
+    else:
+        print('Number of valid parses:',x,'\n')
+
+
+
+
+
+        
